3d1354ec5bfd2acf50d1e342ae8a04562a99e61c
Tristen <chentao3360753@gmail.com>
2015-09-16 13:55:23 +0800

    关闭避免UIButton快速重复点击功能

--- YMFramework/YMUI/UIButton+YMHitAdditions.m
+++ YMFramework/YMUI/UIButton+YMHitAdditions.m
@@ -16,60 +16,6 @@
 
 const static NSString *kYM_HitFrameAdditionsKey = @"kYMHitFrameAdditionsKey";
 
-const static NSString *kYM_AcceptEventInterval = @"kYM_AcceptEventIntervalKey";
-
-const static NSString *kYM_ignoreEvent = @"kYM_ignoreEventKey";
-
-const static NSTimeInterval kYM_DefaultEventInterval = 1.5;
-
-+ (void)load
-{
-    Method a = class_getInstanceMethod(self, @selector(sendAction:to:forEvent:));
-    Method b = class_getInstanceMethod(self, @selector(ym_button_sendAction:to:forEvent:));
-    method_exchangeImplementations(a, b);
-}
-
-#pragma mark - public methods
-
-- (void)ym_changeHitFrameEdgeInsets:(UIEdgeInsets)edgeInsets
-{
-	[self setYm_HitFrameEdgeInsets:edgeInsets];
-}
-
-#pragma mark - private methods
-
-- (void)ym_button_sendAction:(SEL)action
-                          to:(id)target
-                    forEvent:(UIEvent *)event
-{
-    if(![self respondsToSelector:@selector(ym_ignoreEvent)]
-       || ![self respondsToSelector:@selector(ym_button_sendAction:to:forEvent:)]) {
-        
-        [self sendAction:action
-                      to:target
-                forEvent:event];
-        
-        return;
-    }
-    
-    if (self.ym_ignoreEvent) return;
-    
-    NSTimeInterval interval = kYM_DefaultEventInterval;//默认间隔时间
-    if (self.ym_AcceptEventInterval) {
-        interval = [self.ym_AcceptEventInterval doubleValue];
-    }
-    
-    self.ym_ignoreEvent = YES;
-    [self performSelector:@selector(setYm_ignoreEvent:)
-               withObject:@(NO)
-               afterDelay:interval];
-    
-    
-    [self ym_button_sendAction:action
-                            to:target
-                      forEvent:event];
-}
-
 #pragma mark - getters and setters
 
 - (void)setYm_HitFrameEdgeInsets:(UIEdgeInsets)ym_HitFrameEdgeInsets
@@ -104,26 +50,6 @@ const static NSTimeInterval kYM_DefaultEventInterval = 1.5;
 	CGRect hitFrame = UIEdgeInsetsInsetRect(relativeFrame, self.ym_HitFrameEdgeInsets);
 	
 	return CGRectContainsPoint(hitFrame, point);
-}
-
-- (NSNumber *)ym_AcceptEventInterval
-{
-    return objc_getAssociatedObject(self, &kYM_AcceptEventInterval);
-}
-
-- (void)setYm_AcceptEventInterval:(NSNumber *)ym_AcceptEventInterval
-{
-    objc_setAssociatedObject(self, &kYM_AcceptEventInterval, ym_AcceptEventInterval, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (BOOL)ym_ignoreEvent
-{
-    return [objc_getAssociatedObject(self, &kYM_ignoreEvent) boolValue];
-}
-
-- (void)setYm_ignoreEvent:(BOOL)ym_ignoreEvent
-{
-    objc_setAssociatedObject(self, &kYM_ignoreEvent, @(ym_ignoreEvent), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
