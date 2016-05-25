0583b94234edf5f1cfb5a50f8f2ae05f831b96af
TristenChen <TristenChen@gmail.com>
2016-05-25 15:43:25 +0800

    升级Bugtags 1.26

--- example/ExampleForYMFramework/ExampleForYMFramework/YMFramework/BaseVender/Versions.h
+++ example/ExampleForYMFramework/ExampleForYMFramework/YMFramework/BaseVender/Versions.h
@@ -1,9 +1,7 @@
-//IPaynowSDK    X.X.X	X
-
 //YYModel       1.0.3   https://github.com/ibireme/YYModel
 
 //Masonry       1.0.0   https://github.com/SnapKit/Masonry
 
-//Bugtags       1.2.5   https://work.bugtags.com
+//Bugtags       1.2.6   https://work.bugtags.com
 
-//Bugtags       1.3.1   https://github.com/steipete/Aspects
+//Aspects       1.3.1   https://github.com/steipete/Aspects
