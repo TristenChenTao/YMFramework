6e7962e1f01df9b8dba88ee00b7649e11452e5d3
TristenChen <TristenChen@gmail.com>
2016-06-14 15:00:36 +0800

    升级SDWebImage 至 3.8.1

--- src/YMFramework/Vender/Versions.h
+++ src/YMFramework/Vender/Versions.h
@@ -5,15 +5,15 @@
  QQSDK   		2.9.5		http://wiki.open.qq.com/wiki/mobile/SDK下载
  WechatSDK		1.7.1		https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1419319164&token=&lang=zh_CN
  WeiboSDK		3.1.4		https://github.com/sinaweibosdk/weibo_ios_sdk
- WebP			0.5.0		https://github.com/webmproject/libwebp （此版本升级需要依赖SDWebImage的版本支持）
+ WebP			0.5.0		https://github.com/webmproject/libwebp
  
 //////////////////////////////////////源码有改动//////////////////////////////////////
- AFNetworking	3.1.0		https://github.com/AFNetworking/AFNetworking
+ AFNetworking	 3.1.0		https://github.com/AFNetworking/AFNetworking
  SVProgressHUD   1.1.3		   https://github.com/TransitApp/SVProgressHUD
- SDWebImage	3.7.6		https://github.com/rs/SDWebImage
- STWebPDecoder	Null		https://github.com/cysp/STWebPDecoder
- System-Services    1.1.1   	https://github.com/Shmoopi/iOS-System-Services
- MJRefresh		3.1.0		https://github.com/CoderMJLee/MJRefresh
+ SDWebImage      3.8.1		https://github.com/rs/SDWebImage
+ STWebPDecoder	 Null		https://github.com/cysp/STWebPDecoder
+ System-Services 1.1.1   	https://github.com/Shmoopi/iOS-System-Services
+ MJRefresh		 3.1.0		https://github.com/CoderMJLee/MJRefresh
 
  
 //////////////////////////////////////内部使用//////////////////////////////////////
