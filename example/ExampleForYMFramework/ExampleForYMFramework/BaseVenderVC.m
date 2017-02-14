a2814aa0db5a7fcaaf6912a2766e5bf4f7318c61
Tristen陈涛 <TristenChenTao@gmail.com>
2017-02-14 11:13:04 +0800

    去除 BaseVender

--- example/ExampleForYMFramework/ExampleForYMFramework/BaseVenderVC.m
+++ example/ExampleForYMFramework/ExampleForYMFramework/BaseVenderVC.m
@@ -7,8 +7,6 @@
 //
 
 #import "BaseVenderVC.h"
-#import "Masonry.h"
-#import "Aspects.h"
 
 @interface BaseVenderVC()
 
@@ -20,37 +18,9 @@
 - (void)viewDidLoad
 {
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    [self testMasonry];
-    
-    [self testAspects];
-
+        
     [super viewDidLoad];
     
 }
-
-- (void)testMasonry
-{
-    UILabel *viewForMasonry = [[UILabel alloc] init];
-    viewForMasonry.text = @"测试Masonry";
-
-    
-    [self.view addSubview:viewForMasonry];
-    
-    [viewForMasonry mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(250, 50));
-        make.top.equalTo(@20);
-        make.centerX.equalTo(self.view);
-    }];
-}
-
-- (void)testAspects
-{
-    [UIViewController aspect_hookSelector:@selector(viewWillAppear:)
-                              withOptions:AspectPositionAfter
-                               usingBlock:^(id<AspectInfo> info, BOOL animated) {
-                                   UIViewController *controller = [info instance];
-                                   controller.view.backgroundColor = [UIColor grayColor];
-                               } error:NULL];}
 
 @end
