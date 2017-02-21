a5e187f6b5ec4d0990c44aa86085cc8865727497
Tristen陈涛 <TristenChenTao@gmail.com>
2017-02-21 15:38:45 +0800

    在打包模式下需要关闭 Bitcode

--- example/ExampleForYMFramework/ExampleForYMFramework/YMFramework/BaseVender/Versions.h
+++
@@ -1,7 +0,0 @@
-//YYModel       1.0.3   https://github.com/ibireme/YYModel
-
-//Masonry       1.0.0   https://github.com/SnapKit/Masonry
-
-//Bugtags       1.2.6   https://work.bugtags.com
-
-//Aspects       1.4.2   https://github.com/steipete/Aspects
