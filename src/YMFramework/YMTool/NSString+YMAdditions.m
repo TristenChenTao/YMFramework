6d06c67c433138a4cb3079eee58491b5042a0a20
Tristen陈涛 <TristenChenTao@gmail.com>
2017-04-21 09:29:36 +0800

    修改手机号判断规则

--- src/YMFramework/YMTool/NSString+YMAdditions.m
+++ src/YMFramework/YMTool/NSString+YMAdditions.m
@@ -118,9 +118,10 @@
         return NO;
     }
     
-    NSString *MOBILE = @"^1(3[0-9]|4[57]|5[0-35-9]|8[0-9]|70)\\d{8}$";
-    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSString *regex =@"[0-9]*";
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
     return [regextestmobile evaluateWithObject:self];
+
 }
 
 @end
